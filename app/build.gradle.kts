d54e002884ee56c7d21ec2c73bd1852bccbc90e7
Farhad Beigirad <f.beigirad@gmail.com>
2025-03-11 20:24:01 +0330

    initial project

---
+++ app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlin)
+}
+
+android {
+    namespace = "ir.beigirad.admediation.example"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "ir.beigirad.admediation.example"
+        minSdk = 19
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+}
---
+++ app/src/main/java/ir/beigirad/admediation/example/MainActivity.kt
@@ -0,0 +1,10 @@
+package ir.beigirad.admediation.example
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
