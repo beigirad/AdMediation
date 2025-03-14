9b76e76c1637e75ba8110c6b2df11614f1e494a9
Farhad Beigirad <f.beigirad@gmail.com>
2025-03-15 00:20:45 +0330

    add kover as test aggregator

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,13 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.kotlinxSerialization) apply false
+    alias(libs.plugins.kover)
+}
+
+// run ./gradlew koverHtmlReport to generate an aggregated test coverage result for whole project
+kover {
+    merge {
+        val ignoredModules = setOf("example")
+        allProjects { project -> project.name !in ignoredModules }
+    }
 }
